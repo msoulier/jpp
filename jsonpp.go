5a1394f774ea645a4a27e8d9dcdde5c751d45d93
Michael P. Soulier <msoulier@digitaltorque.ca>
2024-12-23 17:31:37 -0500

    Reverting

--- jsonpp.go
+++ jsonpp.go
@@ -5,38 +5,19 @@ import (
     "os"
     "bufio"
     "io"
-    "io/ioutil"
     )
 
 func main() {
-    objmap := make(map[string]interface{})
-    var reader *bufio.Reader = nil
+    objmap := make(map[string]string)
+    buffer := make([]byte, 4096)
+    reader := bufio.NewReader(os.Stdin)
 
-    if len(os.Args) > 1 {
-        if os.Args[1] == "-h" {
-            os.Stdout.WriteString("Usage: jsonpp [input file, - or blank for stdin]\n")
-            os.Exit(0)
-        }
-        if os.Args[1] == "-" {
-            reader = bufio.NewReader(os.Stdin)
-        } else {
-            // Open the file.
-            if infile, err := os.Open(os.Args[1]); err != nil {
-                panic(err)
-            } else {
-                reader = bufio.NewReader(infile)
-            }
-        }
-    } else {
-        reader = bufio.NewReader(os.Stdin)
-    }
-
-    if buffer, err := ioutil.ReadAll(reader); err != nil {
+    if nbytes, err := reader.Read(buffer); err != nil {
         if err != io.EOF {
             panic(err)
         }
     } else {
-        if err := json.Unmarshal(buffer, &objmap); err != nil {
+        if err := json.Unmarshal(buffer[:nbytes], &objmap); err != nil {
             panic(err)
         }
     }
@@ -46,10 +27,4 @@ func main() {
         os.Stdout.Write(b)
         os.Stdout.WriteString("\n")
     }
-    /*
-     * To pull a value out it is done like so:
-     * details_map := objmap["details"].(map[string]interface{})
-     * note: value is an interface{} at this point
-     * active_calls := details_map["active_calls"]
-     */
 }

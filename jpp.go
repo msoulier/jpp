d2f960b3dd38cef296a5963907411ccc43594683
Michael P. Soulier <msoulier@digitaltorque.ca>
2024-12-23 20:06:15 -0500

    Now handling arbitrary json

--- jpp.go
+++ jpp.go
@@ -1,30 +1,58 @@
 package main
 
 import (
-    "encoding/json"
     "os"
     "bufio"
+    "bytes"
     "io"
-    )
+    "github.com/Jeffail/gabs/v2"
+    "github.com/charmbracelet/log"
+    "fmt"
+    "flag"
+)
+
+const (
+    BUFSIZE int = 4096
+)
+
+var (
+    debug bool = false
+)
+
+func init() {
+    flag.BoolVar(&debug, "debug", false, "Debug logging")
+    flag.Parse()
+}
 
 func main() {
-    objmap := make(map[string]string)
-    buffer := make([]byte, 4096)
+    var buffer bytes.Buffer
     reader := bufio.NewReader(os.Stdin)
+    bytes_count := 0
+    if debug {
+        log.SetLevel(log.DebugLevel)
+    }
 
-    if nbytes, err := reader.Read(buffer); err != nil {
-        if err != io.EOF {
-            panic(err)
+    // Read all input data, grow as required
+    for {
+        tempbuf := make([]byte, BUFSIZE)
+        nbytes, err := reader.Read(tempbuf)
+        if err != nil {
+            if err != io.EOF {
+                panic(err)
+            } else {
+                break
+            }
         }
-    } else {
-        if err := json.Unmarshal(buffer[:nbytes], &objmap); err != nil {
-            panic(err)
+        log.Debugf("read %d bytes", nbytes)
+        bytes_count += nbytes
+        buffer.Write(tempbuf)
+        if nbytes < BUFSIZE {
+            break
         }
     }
-    if b, err := json.MarshalIndent(objmap, "", "\t"); err != nil {
+    j, err := gabs.ParseJSON(buffer.Bytes()[:bytes_count])
+    if err != nil {
         panic(err)
-    } else {
-        os.Stdout.Write(b)
-        os.Stdout.WriteString("\n")
     }
+    fmt.Printf(j.StringIndent("", "    "))
 }
